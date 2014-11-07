dded4237309816124eb5277f5689ae873f576022
Pat Allan <pat@freelancing-gods.com>
2014-11-07 17:05:15 +1100

    Sean

--- app.rb
+++ app.rb
@@ -56,7 +56,7 @@ end
 
 get '/2015' do
   @title = :home
-  @speaker = %w( keithpitty.jpg laurenvoswinkel.jpg philiparndt.jpg konstantingredeskoul.png johndalton.jpg shevauncoker.jpg ).sample #johnbarton.jpg erikmichaelsober.jpg
+  @speaker = %w( keithpitty.jpg laurenvoswinkel.jpg philiparndt.jpg konstantingredeskoul.png johndalton.jpg shevauncoker.jpg seanmarcia.jpg ).sample #johnbarton.jpg erikmichaelsober.jpg
     # josspaling.jpg scottfeinberg.jpg
     # sabrinaleandro.jpg seanmarcia.jpg ).sample
   haml :"2015/home", :layout => :"2015/layout"

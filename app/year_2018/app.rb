91f1eec58f31890f6e1092a532704b494a7a06d6
Nicholas Bruning <nicholas@bruning.com.au>
2018-02-22 17:40:13 +1100

    adds sponsors to schedule

--- app/year_2018/app.rb
+++ app/year_2018/app.rb
@@ -143,15 +143,24 @@ module RubyConf
     end
 
     class SocialEvent < ScheduledEvent
-      attr_reader :description
+      attr_reader :description, :sponsor, :sponsor_link
 
-      def initialize(description:, **args)
+      def initialize(description:, sponsor:, sponsor_link:, **args)
         super(**args)
         @description = description
+        @sponsor = sponsor
+        @sponsor_link = sponsor_link
       end
 
       def template_name
         :'2018/schedule/_social_event'
+      end
+
+      def sponsored?
+        !sponsor.nil? &&
+        sponsor.strip.length > 0 &&
+        !sponsor_link.nil? &&
+        sponsor_link.strip.length > 0
       end
     end
 

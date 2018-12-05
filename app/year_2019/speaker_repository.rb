703d226379da243e63cfff1905dd0a239dd455f3
Pat Allan <pat@freelancing-gods.com>
2018-12-05 22:12:37 +1100

    MCs

--- app/year_2019/speaker_repository.rb
+++ app/year_2019/speaker_repository.rb
@@ -15,7 +15,20 @@ module RubyConf
         @speakers
       end
 
+      def via_invite
+        @speakers.select { |speaker| speaker.type == "invitation" }
+      end
+
+      def via_cfp
+        @speakers.select { |speaker| speaker.type == "cfp" }
+      end
+
+      def mcs
+        @speakers.select { |speaker| speaker.type == "mc" }
+      end
+
       private
+
       attr_reader :speakers
     end
   end

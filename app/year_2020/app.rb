549aa10ba800801db67ecba01ed763516fc1f102
Amanda <amanda.chen@connect.qut.edu.au>
2019-08-09 16:41:02 +1000

    Add schedule and location pages

--- app/year_2020/app.rb
+++ app/year_2020/app.rb
@@ -9,12 +9,14 @@ module RubyConf
 
         app.namespace '/2020' do
             get '/?' do
-                @title = :home
+                @title = 'Home'
                 haml :"2020/home", :layout => :"2020/layout"
             end
 
             get '/schedule' do
                 @title = 'Schedule'
+                @lead = 'We have got a gem-packed, there is something for everyone' 
+                haml :"2020/schedule", :layout => :"2020/layout"
             end
 
             get '/:page_name' do

d1b0fb831cf3a86b78328f44d4bf3e63a1a3a5e1
Amanda <amanda.chen@connect.qut.edu.au>
2019-10-23 17:22:03 +1000

    WIP: Adding Schedule for Social Events

--- app/year_2020/schedule_repository.rb
+++ app/year_2020/schedule_repository.rb
@@ -1,35 +1,34 @@
 module RubyConf
-    module Year2020
-      class ScheduleRepository
-        TYPE_MAP = {
-          social_event: SocialEvent,
-          break: BreakEvent,
-          session: SessionEvent
-        }
-  
-        def initialize(schedule:)
-          @events = schedule.collect do |data|
-            create(
-              data.inject({}) do |memo, (key, value)|
-                memo[key.to_sym] = value
-                memo
-              end
-            )
-          end
+module Year2020
+    class ScheduleRepository
+      TYPE_MAP = {
+        social_event: SocialEvent,
+        break: BreakEvent,
+        session: SessionEvent
+      }
+
+      def initialize(schedule:)
+        @events = schedule.collect do |data|
+          create(
+            data.inject({}) do |memo, (key, value)|
+              memo[key.to_sym] = value
+              memo
+            end
+          )
         end
-  
-        def for(day:)
-          events.select { |event| event.day == day }
-        end
-  
-        private
-  
-        attr_reader :events
-  
-        def create(type:, **args)
-          TYPE_MAP[type.to_sym] && TYPE_MAP[type.to_sym].new(**args) || fail("Unknown scheduled event type: '#{type}'")
-        end
+      end
+
+      def for(day:)
+        events.select { |event| event.day == day }
+      end
+
+      private
+
+      attr_reader :events
+
+      def create(type:, **args)
+        TYPE_MAP[type.to_sym] && TYPE_MAP[type.to_sym].new(**args) || fail("Unknown scheduled event type: '#{type}'")
       end
     end
   end
-  
+end
